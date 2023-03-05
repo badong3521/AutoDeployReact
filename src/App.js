f3c8812d09a12685ab136b152a9adc39a73c81f6
BaDong <dongba352001@gmail.com>
2023-03-05 16:55:29 +0700

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import { React } from 'react';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return <h1>Xin chào = hello</h1>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Edit <code>src/App.js</code> and save to reload.
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
