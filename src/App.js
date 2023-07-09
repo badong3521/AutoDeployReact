928c02536c8896b77c38ca7c19b7eb6c8eb017bd
BaDong <dongba352001@gmail.com>
2023-07-09 17:25:10 +0700

    ag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [orders, setOrder] = useState('');
+  useEffect(() => {
+    const data = axios({
+      method: 'get',
+      url: 'http://localhost:3002/api/count/orders',
+      responseType: 'stream',
+    })
+      .then((data) => {
+        console.log('PROMISE: ', data.data);
+        return data.data;
+      })
+      .catch((error) => {
+        console.log('ERROR', error);
+      });
+    setOrder(data);
+  }, []);
+  console.log('orders', orders);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Em 3 deploy</h1>
+        <h1>Em 3 deploy again</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +36,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
